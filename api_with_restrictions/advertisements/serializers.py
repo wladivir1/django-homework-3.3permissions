7123600e644281818ba6b132fb491df9cae9fa56
VV <wladivir1@mail.ru>
2023-12-22 01:20:37 +0500

    Initial commit

---
+++ api_with_restrictions/advertisements/serializers.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+
+        # Простановка значения поля создатель по-умолчанию.
+        # Текущий пользователь является создателем объявления
+        # изменить или переопределить его через API нельзя.
+        # обратите внимание на `context` – он выставляется автоматически
+        # через методы ViewSet.
+        # само поле при этом объявляется как `read_only=True`
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        if self.context["request"].method == "POST":
+            if Advertisement.objects.filter(status="OPEN").count() > 10:
+                raise serializers.ValidationError("Максимальное количество открытых объявлений - 10")
+        # if self.context["request"].method == "PATCH":
+        #     if self.instance.creator != self.context["request"].user:
+        #         raise serializers.ValidationError("Нельзя редактировать чужое объявление")
+                
+        return data
+
+    
---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,30 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from .permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
+from .serializers import AdvertisementSerializer
+from .filters import AdvertisementFilter
+from .models import Advertisement
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
+        
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(),IsAdminOrReadOnly()]
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        
+        return []
+    
+    
+        
+    
+    
